39a08d0a925757c94a5e8b86fa7ef89f8da9bb17
Sam Williams <sam@ukethat.com>
2017-08-17 06:24:18 +0000

    added flash messages to choosing plan

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+   
+   before_action :select_plan, only: :new
+   
    def create
       super do |resource|
          if params[:plan]
@@ -13,4 +16,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
          end
       end
    end
+   
+   private
+      def select_plan
+         unless (params[:plan] == '1' || params[:plan] == '2' || params[:plan] == '3')
+            flash[:notice] = "Please select a valid plan"
+            redirect_to root_url
+         end
+      end
 end
