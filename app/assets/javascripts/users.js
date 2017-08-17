7c509c35be8a35e8aaf13ed3f6021fe10318821e
Sam Williams <sam@ukethat.com>
2017-08-17 05:36:02 +0000

    added stripe response handler and error handling js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -5,23 +5,68 @@ $(document).on('turbolinks:load', function() {
     var cenaForm = $('#cena_form');
     var submitBtn = $('#form-submit-btn')
     
-    Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
+    Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content') );
 
-    submitBtn.click(function(event)) {
+    submitBtn.click(function(event) {
         //prevent default submission behavior
         event.preventDefault();
+        submitBtn.val("Processing").prop('disabled', true);
         
         var ccNum = $('#card_number').val(),
             cvcNum = $('#card_code').val(),
             expMonth = $('#card_month').val(),
             expYear = $('#card_year').val();
         
-        Stripe.createToken({
+        //Use Stripe JS library to check for card errors.
+        var error = false;
+        
+        //Validate card number
+        if (!Stripe.card.validateCardNumber(ccNum)) {
+            error = true;
+            alert('The credit card number appears to be invalid');
+        }
+        
+        if (!Stripe.card.validateCVC(cvcNum)) {
+            error = true;
+            alert('The cvc number appears to be invalid');
+        }
+        
+        if (!Stripe.card.validateExpiry(expMonth, expYear)) {
+            error = true
+            alert('The expiration date appears to be invalid');
+        }
+        
+        if (error) {
+            submitBtn.prop('disabled', false).val("Submit");
+        } 
+        else {
+            Stripe.createToken({
             number : ccNum, 
             cvc: cvcNum,
             exp_month: expMonth,
             exp_year: expYear
-        }, stripeResponseHandler);
+            }, stripeResponseHandler);
+        }
+        
+        
+        return false;
+    });
+    
+    function stripeResponseHandler(status, response) {
+        //Get token from response
+        var token = response.id;
+        
+        //Inject the card token in a hidden field.
+        //Differentiating because if cenaForm is found we need to use it, if proForm is found, we need to use it, and if its default, it doesn't matter
+        if (cenaForm.length > proForm.length) {
+            cenaForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
+            cenaForm.get(0).submit();
+        }
+        else {
+            proForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
+            proForm.get(0).submit();
+            
+        }
     }
     //Stripe will return a card toekn.
     //Inject card token as hidden field into form.
